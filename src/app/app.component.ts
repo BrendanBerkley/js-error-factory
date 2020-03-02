3908b2f6b16d1bbd46f8ff13c3cc98b1faca1694
Brendan <brendan@pop-os.localdomain>
2020-03-02 13:22:34 -0500

    all usable error types, a bulk error generator option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   throwError() {
-    throw "an error string";
+    throw "A Generic Error";
+  }
+
+  throwEvalError() {
+    throw new EvalError('MDN says "EvalError is not used in the current ECMAScript specification and will thus not be thrown by the runtime. However, the object itself remains for backwards compatibility with earlier versions of the specification."');
+  }
+
+  checkRange(i: number) {
+    if (!(i >= 0 && i <= 10) ) {
+      throw new RangeError("The number must be between 0 and 10.")
+    }
+  }
+
+  throwReferenceError() {
+    // @ts-ignore
+    let a = undefinedVariable;
+  }
+
+  throwSyntaxError() {
+    eval('nope');
+  }
+
+  throwUriError() {
+    decodeURIComponent('%');
+  }
+
+  hashCode = () => {
+    const hash = [];
+    for (let i = 0; i < 6; i++) {
+      const randomChar = Math.floor(Math.random() * Math.floor(16)).toString(16);
+      hash.push(randomChar);
+    }
+    return hash.join('');
+  }
+
+  throwMultipleErrors = (numberOfErrors: number) => {
+    for (let i = 0; i < numberOfErrors; i++) {
+      const errorEvent = new ErrorEvent('error');
+      window.dispatchEvent(errorEvent);
+    }
+  }
+
+  throwGenericError() {
+    throw new Error('Bulk Generic Error! ' + this.hashCode());
+  }
+
+  ngOnInit() {
+    window.addEventListener('error', e => this.throwGenericError());
   }
 }
