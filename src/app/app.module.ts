3124044154e7c1cdfb1502c843325d4c905a803f
David Burke <david@davidmburke.com>
2022-07-11 21:41:24 -0400

    Update packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, Injectable, ErrorHandler } from "@angular/core";
-import { HttpErrorResponse } from "@angular/common/http";
-import * as Sentry from "@sentry/browser";
+import { NgModule, ErrorHandler } from "@angular/core";
+import * as Sentry from "@sentry/angular";
 import { Integrations as TracingIntegrations } from "@sentry/tracing";
 import { AppRoutingModule } from "./app-routing.module";
 
@@ -28,56 +27,6 @@ Sentry.init({
   },
 });
 
-@Injectable()
-export class SentryErrorHandler implements ErrorHandler {
-  constructor() {}
-
-  extractError(error) {
-    // Try to unwrap zone.js error.
-    // https://github.com/angular/angular/blob/master/packages/core/src/util/errors.ts
-    if (error && error.ngOriginalError) {
-      error = error.ngOriginalError;
-    }
-
-    // We can handle messages and Error objects directly.
-    if (typeof error === "string" || error instanceof Error) {
-      return error;
-    }
-
-    // If it's http module error, extract as much information from it as we can.
-    if (error instanceof HttpErrorResponse) {
-      // The `error` property of http exception can be either an `Error` object, which we can use directly...
-      if (error.error instanceof Error) {
-        return error.error;
-      }
-
-      // ... or an`ErrorEvent`, which can provide us with the message but no stack...
-      if (error.error instanceof ErrorEvent) {
-        return error.error.message;
-      }
-
-      // ...or the request body itself, which we can use as a message instead.
-      if (typeof error.error === "string") {
-        return `Server returned code ${error.status} with body "${error.error}"`;
-      }
-
-      // If we don't have any detailed information, fallback to the request message itself.
-      return error.message;
-    }
-
-    // Skip if there's no error, and let user decide what to do with it.
-    return null;
-  }
-
-  handleError(error) {
-    const extractedError = this.extractError(error) || "Handled unknown error";
-    const eventId = Sentry.captureException(extractedError);
-    Sentry.captureException(error.originalError || error);
-    // Sentry.showReportDialog({ eventId });
-    console.error(error);
-  }
-}
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -87,7 +36,7 @@ export class SentryErrorHandler implements ErrorHandler {
     Tracing3Component,
   ],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
+  providers: [{ provide: ErrorHandler,  useValue: Sentry.createErrorHandler()}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
